f42150c9aa044801ab257fe479c562261973c054
Amine Bouobida <aminebouobida6@gmail.com>
2024-01-13 23:48:22 +0100

    first commit

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -55,6 +55,24 @@ export default function Nav(params) {
               </div>
             </div>
           </div>
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    "text-gray-600 hover:bg-gray-300 hover:text-gray-900",
+                    "block rounded-md px-3 py-2 text-base font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
